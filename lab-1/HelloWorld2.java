da68578bd66dd9316d3881e69ae0c0331f3ab593
Callisto <hesscallisto@gmail.com>
2020-01-26 15:51:40 -0500

    Submission for Lab 1

---
+++ lab-1/HelloWorld2.java
@@ -0,0 +1,12 @@
+// file name matches public class identifier
+public class HelloWorld2{
+
+    // main method, ignition, accepts string args
+    public static void main(String[] args){
+
+        System.out.print("Hello ");
+        System.out.print(args[0] + " ");
+        System.out.print(args[1] + ". ");
+        System.out.println("Nice work processing the arguments.");
+    }
+}
